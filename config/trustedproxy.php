e533c9166f2f9a5f0a710327a839aa19bb51ed2a
eymannajihah <eymannajihah07@gmail.com>
2026-01-18 10:20:24 +0800

    Delete trustedproxy.php

--- config/trustedproxy.php
+++
@@ -1,7 +0,0 @@
-return [
-    'proxies' => '*',
-    'headers' => Illuminate\Http\Request::HEADER_X_FORWARDED_FOR
-        | Illuminate\Http\Request::HEADER_X_FORWARDED_HOST
-        | Illuminate\Http\Request::HEADER_X_FORWARDED_PORT
-        | Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO,
-];
